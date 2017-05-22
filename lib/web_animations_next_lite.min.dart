02fca107688f55799c6dce21a66cbc2dda800067
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-05-22 12:52:43 +0200

    added web animations

---
+++ lib/web_animations_next_lite.min.dart
@@ -0,0 +1,10 @@
+import 'package:polymer_element/polymer_element.dart' show BowerImport;
+
+
+
+/**
+ **/
+@BowerImport(ref:'web-animations-js#2.2.5',import:"web-animation/web-animations-next-lite.min.html",name:'web-animation')
+class _ {
+
+}
